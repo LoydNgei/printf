44104b18801e009f784e5d65502dd75459b597cc
loyd ngei <loydngei@gmail.com>
2022-10-20 13:06:01 -0700

    output

--- _printf.c
+++ _printf.c
@@ -1,47 +1,48 @@
 #include "main.h"
 
 /**
- *_printf - Print a formatted string
- *@format: format string
- *Return: number of characters printed
+ *_printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print()
+ *function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
  */
 int _printf(const char *format, ...)
 {
-int count = 0;
-va_list list;
-char *pointer, *start;
-param_func flags = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-va_start(list, format);
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flag_t flags = {0, 0, 0};
 
-if (!format || (format[0] == '%' && !format[1]))
-	return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-	return (-1);
-for (pointer = (char *)format; *pointer; pointer++)
-{
-init_params(&flags, list);
-if (*pointer != '%')
-{
-count += _putchar(*pointer);
-continue;
-}
-start = pointer;
-pointer++;
-while (get_flags(pointer, &flags))
-{
-pointer++;
-}
-pointer = get_width(pointer, &flags, list);
-pointer = get_precision(pointer, &flags, list);
-if (get_mods(pointer, &flags))
-pointer++;
-if (!func_parse(pointer))
-count += print_range(start, pointer,
-flags.l_mod || flags.h_mod ? pointer - 1 : 0);
-else
-count += print_func(pointer, list, &flags);
-}
-_putchar(-1);
-va_end(list);
-return (count);
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				countinue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
